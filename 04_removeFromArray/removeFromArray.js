c712c5abd9259c549a5070c7b4c252d369939fcc
CodeWithNas <183351791+CodeWithNas@users.noreply.github.com>
2025-05-05 15:19:12 +0300

    Complete JS exercises from TOP 01-12

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+ let result = [];
+
+ for (let i = 0; i < array.length; i++) {
+    let currentItem = array[i];
+
+    if (!args.includes(currentItem)) {
+        result.push(currentItem);
+    }
+ }
+ return result;
+
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        const currentYear = new Date().getFullYear();
+    
+        const personDeath = person.yearOfDeath || currentYear;
+        const personAge = personDeath - person.yearOfBirth;
+    
+        const oldestDeath = oldest.yearOfDeath || currentYear;
+        const oldestAge = oldestDeath - oldest.yearOfBirth;
+    
+        return personAge > oldestAge ? person : oldest;
+      });
 };
 
 // Do not edit below this line
